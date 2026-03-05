0f0031cd5be718e5217f52a97bc59aed2edd4057
Chirag Gupta <piyushg.2648@gmail.com>
2026-03-06 00:26:31 +0530

    changing colors

--- digitalclock.py
+++ digitalclock.py
@@ -3,7 +3,7 @@ from time import strftime
 
 root = tk.Tk()
 root.title("Digital clock")
-root.configure(bg="yellow")
+root.configure(bg="black")
 
 def time():
     current_time = strftime('%H:%M:%S %p')
@@ -14,10 +14,10 @@ def time():
 
     time_label.after(1000, time)
 
-time_label = tk.Label(root, font=('calibri', 60, 'bold'), background='yellow', foreground='black')
+time_label = tk.Label(root, font=('calibri', 60, 'bold'), background='black', foreground='red')
 time_label.pack(anchor='center')
 
-date_label = tk.Label(root, font=('calibri', 35, 'bold'), background='yellow', foreground='black')
+date_label = tk.Label(root, font=('calibri', 35, 'bold'), background='black', foreground='red')
 date_label.pack(anchor='center')
 
 time()
