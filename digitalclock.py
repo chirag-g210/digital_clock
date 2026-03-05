4ee50ea6110bf1fef3f6a7f5b4942e37d3cb059f
Chirag Gupta <piyushg.2648@gmail.com>
2026-03-06 00:52:00 +0530

    fixing AM/PM display issue

--- digitalclock.py
+++ digitalclock.py
@@ -6,19 +6,28 @@ root.title("Digital clock")
 root.configure(bg="black")
 
 def time():
-    current_time = strftime('%H:%M:%S %p')
+    current_time = strftime('%H:%M:%S')
+    current_ampm = strftime('%p')
     current_date = strftime('%d/%m/%Y')
-    
+
     time_label.config(text=current_time)
+    ampm_label.config(text=current_ampm)
     date_label.config(text=current_date)
 
-    time_label.after(1000, time)
+    root.after(1000, time)
 
-time_label = tk.Label(root, font=('Digital-7 Mono', 50, 'bold'), background='black', foreground='#00FFFF')
-time_label.pack(anchor='center')
+# frame for time
+time_frame = tk.Frame(root, bg="black")
+time_frame.pack()
 
-date_label = tk.Label(root, font=('Digital-7 Mono', 30, 'bold'), background='black', foreground='#00FFFF')
-date_label.pack(anchor='center')
+time_label = tk.Label(time_frame, font=('Digital-7 Mono', 50), bg='black', fg='#00FFFF')
+time_label.pack(side="left")
+
+ampm_label = tk.Label(time_frame, font=('Digital-7 Mono', 25), bg='black', fg='#00FFFF')
+ampm_label.pack(side="left", padx=5, anchor="n")
+
+date_label = tk.Label(root, font=('Digital-7 Mono', 30), bg='black', fg='#00FFFF')
+date_label.pack()
 
 time()
 
