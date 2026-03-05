1659a8ab3843ccfab7bc30df19e54a7a0cd89038
Chirag Gupta <piyushg.2648@gmail.com>
2026-03-06 00:20:33 +0530

    changing size of date

--- digitalclock.py
+++ digitalclock.py
@@ -3,14 +3,22 @@ from time import strftime
 
 root = tk.Tk()
 root.title("Digital clock")
+root.configure(bg="yellow")
 
 def time():
-    string = strftime('%H:%M:%S %p \n %d/%m/%Y')
-    label.config(text=string)
-    label.after(1000,time)
+    current_time = strftime('%H:%M:%S %p')
+    current_date = strftime('%d/%m/%Y')
+    
+    time_label.config(text=current_time)
+    date_label.config(text=current_date)
 
-label = tk.Label(root, font=('calibri', 50, 'bold'), background='yellow', foreground='black')
-label.pack(anchor='center')
+    time_label.after(1000, time)
+
+time_label = tk.Label(root, font=('calibri', 60, 'bold'), background='yellow', foreground='black')
+time_label.pack(anchor='center')
+
+date_label = tk.Label(root, font=('calibri', 35, 'bold'), background='yellow', foreground='black')
+date_label.pack(anchor='center')
 
 time()
 
